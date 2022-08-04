07a085bb602c235fd6648baa7feaf8aa02bdb2e5
JacobBartlett24 <jacobart@ttu.edu>
2022-08-04 15:03:21 -0500

    Add image counter

--- practice/ImageSlider/app.js
+++ practice/ImageSlider/app.js
@@ -1,12 +1,14 @@
 function switchImage(direction){
-  console.log('cccc');
+  console.log('ss');
 }
 
 function clickableArrows(){
   const arrows = document.getElementsByClassName('arrow');
   for (let i = 0; i< arrows.length; i++){
-    arrows[i].addEventListener('click', switchImage);
+    arrows[i].addEventListener('click', switchImage());
   }
 }
 
-clickableArrows()
+function countImages(){
+  
+}
