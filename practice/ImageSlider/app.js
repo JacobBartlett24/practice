d2e077a8586c8717d6f901b338fbb4f22d8206b1
JacobBartlett24 <jacobart@ttu.edu>
2022-08-05 15:21:47 -0500

    Add slider and a few basic functions

--- practice/ImageSlider/app.js
+++ practice/ImageSlider/app.js
@@ -1,14 +1,71 @@
-function switchImage(direction){
-  console.log('ss');
+function image (src, imageNO){
+  return {
+    src,
+    imageNO
+  }
+}
+
+const imageList = preloadImages();
+
+function switchImage(){
+  let imageFrame = document.getElementById('imageFrame');
+  let currentImage = document.getElementById('currentImage');
+  let newImage = document.createElement('img')
+
+  let currentImageNO = currentImage.getAttribute('imageno');
+
+  newImage = document.createElement('img');
+  newImage.setAttribute('src',`${imageList[currentImageNO+1].src}`);
+  newImage.setAttribute('id','currentImage');
+  newImage.setAttribute('imageno',`${imageList[currentImageNO+1].imageNO}`);
+ 
+  imageFrame.replaceChildren()
 }
 
 function clickableArrows(){
   const arrows = document.getElementsByClassName('arrow');
   for (let i = 0; i< arrows.length; i++){
     arrows[i].addEventListener('click', switchImage());
+    console.log('here');
   }
 }
 
-function countImages(){
-  
+function createSlider(images){
+  let length = images.length;
+  let imageCounter = document.getElementsByClassName('imageCounter'); 
+ 
+
+  for (i = 0; i < length; i++){
+    let dot = document.createElement('span')
+    dot.setAttribute('class','dot')
+    imageCounter[0].appendChild(dot);
+  }
+
 }
+
+createSlider(imageList);
+
+function preloadImages(){
+  const images = [];
+
+  for (let i = 0; i < 8; i++){
+    let stockImage = image('./Image.jpeg', i);
+    images.push(stockImage);
+  }
+
+  return images;
+}
+
+function populateImageFrame(){
+  let imageFrame = document.getElementById('imageFrame');
+
+  firstImage = document.createElement('img');
+  firstImage.setAttribute('src',`${imageList[0].src}`);
+  firstImage.setAttribute('id','currentImage');
+  firstImage.setAttribute('imageno',`${imageList[0].imageNO}`);
+
+  imageFrame.appendChild(firstImage);
+}
+
+populateImageFrame();
+clickableArrows();
